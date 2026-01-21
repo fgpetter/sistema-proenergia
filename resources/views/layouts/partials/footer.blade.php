3c6a10a8adb5f684ef0b2316d74ed962ffa8fb9c
Filipe Petter <fgpetter@gmail.com>
2026-01-21 17:38:46 -0300

    feat: altera dados do rodape

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,13 +2,13 @@
 <footer class="mt-auto footer flex items-center py-5 border-t border-default-200">
     <div class="lg:px-8 px-6 w-full flex md:justify-between justify-center gap-4">
         <div>
+            Proenergia - Sistema de Gestão de Projetos - 
             <script>
                 document.write(new Date().getFullYear())
             </script>
-            © Tailwick
         </div>
         <div class="md:flex hidden gap-2 item-center md:justify-end">
-            Design &amp; Develop by<a class="text-primary" href="https://themesdesign.in/" target="_blank">Themesdesign</a>
+            Desenvolvido por <a class="text-primary" href="https://premiumweb.com.br/" target="_blank">PremiumWeb</a>
         </div>
     </div>
 </footer>
